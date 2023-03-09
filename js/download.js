dda68a39ef06e06d5e8777fd37bd549bd5c36dc5
KImSuim <suim7171@naver.com>
2023-03-09 23:39:37 +0900

    팝업창, 이동바 완성

--- js/download.js
+++ js/download.js
@@ -12,6 +12,12 @@ document.addEventListener('scroll', function () {
   console.log('currentScrollValue is ' + currentScrollValue);
 });
 
+// arrow up 버튼 클릭했을 때 home으로 올라가기
+const receivingNews = document.querySelector('#btn-News');
+receivingNews.addEventListener('click', () => {
+  scrollIntoView('#receivingNews');
+});
+
 // arrow up 버튼 스크롤 될 때 생성
 const arrowUp = document.querySelector('#arrow-up');
 document.addEventListener('scroll', () => {
@@ -27,8 +33,39 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#receivingNews');
 });
 
+// pop-up 버튼 스크롤 될 때 생성
+const popUp = document.querySelector('#pop-up');
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  if (
+    window.scrollY > homeHeight / 2 &&
+    window.scrollY < homeHeight / 2 + 100
+  ) {
+    popUp.classList.add('visible');
+  } else if (window.scrollY < homeHeight / 2) {
+    popUp.classList.remove('visible');
+  }
+});
+
+// pop-up 버튼 클릭했을 때 home으로 올라가기
+const Btn_PopUp = document.querySelector('#btn-pop-up');
+Btn_PopUp.addEventListener('click', () => {
+  scrollIntoView('#receivingNews');
+  popUp.classList.add('close');
+});
+
+// pop-up 버튼 스크롤 될 때 생성
+const btnClose = document.querySelector('#btn-close');
+btnClose.addEventListener('click', () => {
+  popUp.classList.add('close');
+});
+
 //스무스하게 스크롤을 하게 하고 / 스크롤을 하기 위해선 필요한 존재
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  const header = document.querySelector('.header');
+  const navTabs = document.querySelector('.nav-tabs');
+  const offset =
+    scrollTo.offsetTop - header.offsetHeight - navTabs.offsetHeight;
+  window.scrollTo({ top: offset, behavior: 'smooth' });
 }
